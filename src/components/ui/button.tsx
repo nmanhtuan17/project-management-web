f64800dc1d26d285ef1cf8661ada90ea12653574
Nguyễn Mạnh Tuấn <tuanmnguye@northstudio.vn>
2024-11-05 00:33:08 +0700

    :lipstick: button with icon & loading

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,8 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+import { LoadingSpinner } from "@/components/ui/loading-spinner"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
@@ -40,18 +42,27 @@ export interface ButtonProps
   asChild?: boolean
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+interface ButtonWithIconProps extends ButtonProps {
+  icon?: ReactNode;
+  loading?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonWithIconProps>(
+  ({className, variant, size, asChild = false, loading, icon, ...props}, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={loading}
+        className={cn(buttonVariants({variant, size, className}))}
         ref={ref}
         {...props}
-      />
+      >
+        {loading ? <LoadingSpinner className={'w-4 h-4 mr-1'}/> : icon}
+        {props.children}
+      </Comp>
     )
   }
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export {Button, buttonVariants}
