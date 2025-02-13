8103a1b907601f6ca20cef3d5d1e242e677b0a53
Nguyễn Mạnh Tuấn <tuanmnguye@northstudio.vn>
2025-02-13 13:20:35 +0700

    :wrench: add dependencies

--- src/views/tasks/TaskTimeline/index.tsx
+++ src/views/tasks/TaskTimeline/index.tsx
@@ -1,8 +1,22 @@
-export const TaskTimeline = () => {
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
 
+export const TaskTimeline = () => {
+  let tasks: Task[] = [
+    {
+      start: new Date(2020, 1, 1),
+      end: new Date(2020, 1, 2),
+      name: 'Idea',
+      id: 'Task 0',
+      type: 'task',
+      progress: 100,
+      isDisabled: true,
+      styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+    },
+  ];
   return (
     <div>
-
+      <Gantt tasks={tasks} />
     </div>
   )
 }
